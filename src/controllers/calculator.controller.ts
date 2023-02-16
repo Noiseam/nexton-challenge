0607652b0212c3b4fe499718f9b22741907c112a
noiseam <ggwpgallo@gmail.com>
2023-02-16 01:28:45 -0300

    fix: swagger testing

--- src/controllers/calculator.controller.ts
+++ src/controllers/calculator.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags,ApiBody } from '@nestjs/swagger';
 import { CalculatorService } from '../services/calculator.service';
 @ApiTags('calculator')
 @Controller('calculator')
@@ -11,6 +11,17 @@ export class CalculatorController {
   }
   
   @Post()
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        expression: {
+          type: 'string',
+          example: '1 + 2 * (3 - 4) / 5',
+        },
+      },
+    },
+  })
   calculate(@Body() body: { expression: string }): string {
     return this.calculatorService.calculate(body.expression);
   }
